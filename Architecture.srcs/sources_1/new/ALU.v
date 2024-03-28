8a1dc077cb9e94dd67503b4e4a79c57b9880fb55
yahong <yechan0815a@gmail.com>
2024-03-28 15:04:09 +0900

    feat: ALU

--- Architecture.srcs/sources_1/new/ALU.v
+++ Architecture.srcs/sources_1/new/ALU.v
@@ -23,14 +23,19 @@
 module ALU(
     input [31:0] source,
     input [31:0] operand,
-    input [3:0] opcode
+    input [3:0] opcode,
+    output [31:0] result,
+    output c,
+    output n,
+    output z,
+    output v
     );
     
      /* AND 0000, OR 0001, XOR 0010, NOT 0011
         ADD 0100, SUB 0101
-        EXTENDER 0110 COMPARE 0111
+        EXTENDER 0110, COMPARE 0111
         Logical Shift Right 1000, Arithmetic Shift Right 1001, Logical Shift Left 1010
-        RESERVED 1011 1100 1101 1110 1111 */
+        RESERsVED 1011 1100 1101 1110 1111 */
         
     wire [31:0] w_and, w_or, w_xor, w_not, w_add, w_extend, w_compare, w_lsr, w_asr, w_lsl;
     
@@ -55,4 +60,17 @@ module ALU(
         .b(operand),
         .result(w_extend)
     );
+    
+    assign result = (opcode == 4'b0000) ? w_and :
+                    (opcode == 4'b0001) ? w_or :
+                    (opcode == 4'b0010) ? w_xor :
+                    (opcode == 4'b0011) ? w_not :
+                    (opcode == 4'b0100) ? w_add :
+                    (opcode == 4'b0101) ? w_add :
+                    (opcode == 4'b0110) ? w_extend :
+                    (opcode == 4'b0111) ? 32'h0 :
+                    (opcode == 4'b1000) ? w_lsr :
+                    (opcode == 4'b1001) ? w_asr :
+                    (opcode == 4'b1010) ? w_lsl : 32'h0;
+
 endmodule
