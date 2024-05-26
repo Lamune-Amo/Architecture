7ea1435725e2fd8cdf36dacd9cc2ad4b41d35064
yahong <yechan0815@naver.com>
2024-05-26 22:08:10 +0900

    rec

--- Architecture.gen/sources_1/ip/dist_mem_gen_0/dist_mem_gen_0_stub.v
+++ Architecture.gen/sources_1/ip/dist_mem_gen_0/dist_mem_gen_0_stub.v
@@ -2,7 +2,7 @@
 // Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2023.2 (win64) Build 4029153 Fri Oct 13 20:14:34 MDT 2023
-// Date        : Tue May 21 20:47:02 2024
+// Date        : Sun May 26 14:55:17 2024
 // Host        : DESKTOP-TDU015C running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode synth_stub -rename_top dist_mem_gen_0 -prefix
 //               dist_mem_gen_0_ dist_mem_gen_0_stub.v
